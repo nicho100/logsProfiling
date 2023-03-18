e2c493c1299762e9f934fddec0e8e5705e949380
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-03-18 15:04:18 -0300

    Update enviorment.js

--- config/enviorment.js
+++ config/enviorment.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 const args= require("minimist")(process.argv.slice(2))
 //traigo el link de mongo de .env y lo exporto
-export const urlMongo=process.env.URL//uso dotenv para guardar el link de la base de datos
-export const config={
+const urlMongo=process.env.URL//uso dotenv para guardar el link de la base de datos
+ const config={
     puerto:args.p??8080,
-    modo:args.m??fork
+    modo:args.m??"fork"
 }
 //uso minimist para configurar el puerto
+module.exports={urlMongo,config}
