f4e982a937c8c1bf41f8d706b400ac0725c7fbd6
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-03-18 13:10:52 -0300

    Update random.js

--- routes/random.js
+++ routes/random.js
@@ -1,25 +1,24 @@
-import { Router } from "express"
+const { Router } = require("express")
 const routerRandom= Router()
-import{fork} from "child_process"
+const{fork} = require("child_process")
 const forks=fork("./helper/longProcess.js")
+
 routerRandom.get('/randoms/:cant',(req,res)=>{
 const cantidad = req.params.cant
-
 forks.send(cantidad)//uso forks para processo no bloqueante
-forks.on("message",(message)=>{
-    res.end(message.resultadoObjeto)
+forks.on("message",(message)=>{//padre
+    res.end(JSON.stringify(message,null,2))
 })
 
 })
 routerRandom.get('/randoms',(req,res)=>{
     const cantidad = 1000000
-    
     forks.send(cantidad)
     forks.on("message",(message)=>{
-        res.end(message.resultadoObjeto)
+        res.end(`el numero random es ${JSON.stringify(message,null,2)}`)
     })
     
     })
 
 
-export{routerRandom}
+module.exports={routerRandom}
